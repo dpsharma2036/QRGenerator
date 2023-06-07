f94fa1445e27c2520a9d1eeca89c16d831e3c133
dpsha@DESKTOP-EJNNPU <dpsharma2036@gmail.com>
2023-06-06 21:53:20 -0400

    js and html files modified

---
+++ code.js
@@ -0,0 +1,4 @@
+const qrtext = document.getElementById('qr-text');
+const size = document.getElementById('sizes');
+const generator = document.getElementById('generateBtn');
+const downloader = document.getElementById('downloadBtn');
